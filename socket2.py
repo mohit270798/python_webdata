260fd49d11e4ddbe20bc2c494d5cbb642ffa6901
mohit270798 <16ucc056@lnmiit.ac.in>
2018-08-15 10:51:19 +0530

    data from youtube

---
+++ socket2.py
@@ -0,0 +1,19 @@
+import socket
+
+# This is using HTTP 1.0 - not all servers support the oldest protocol
+# Try http://data.pr4e.org/romeo.txt if your server fails.
+
+#url = input('Enter: ')
+#words = url.split('/')
+#host = words[2]
+
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('www.youtube.com', 80))
+mysock.send(('GET https://www.youtube.com HTTP/1.0\r\n\r\n').encode())
+
+while True:
+    data = mysock.recv(512)
+    if (len(data) < 1):
+        break
+    print data.decode()
+mysock.close()
